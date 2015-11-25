7bdef5304aaa2630d713b6b8e5623892f47ad23e
Andrei Rînea <andrei@rinea.ro>
2015-11-25 23:28:47 +0200

    Initial commit

---
+++ Models/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace AngularIntro.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
